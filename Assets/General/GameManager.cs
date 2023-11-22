7214a7dd853b5c733309f563f75fffa6267b7ad5
Pascal <pascal.keller12@gmail.com>
2023-11-22 15:04:04 +0100

    Added GameManager

---
+++ Assets/General/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance is not null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
